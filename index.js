4ead312da0528c1b81da1cda4e8b6a7fdf16aad7
Greg Morgan <gregm9999@gmail.com>
2019-03-21 23:41:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  let x = document.querySelector(selector);
+  return x;
+}
 
+function nestedTarget() {
+  let x = document.querySelector("#nested .target");
+  return x;
+}
+
+function increaseRankBy(n) {
+   let x =document.querySelectorAll('ul.ranked-list li');
+   for (let i =0; i< x.length; i++) {
+     x[i].innerHTML = parseInt(x[i].innerHTML, 10) + n;
+   }
+}
+
+function deepestChild() {
+  let x =document.querySelector("#grand-node div div div div");
+  return x;
+}
